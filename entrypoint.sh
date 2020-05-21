3bda31554ffab20c659dd6c2330df883556531b4
Naveen <59356289+naveenr-btc@users.noreply.github.com>
2020-05-21 10:17:51 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "Running check"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 exec java -jar /checkstyle.jar -c /"${INPUT_CHECKSTYLE_CONFIG}" ./ -f xml \
- | reviewdog -f=checkstyle -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+ | reviewdog -f=checkstyle -name="checkstyle" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
 
