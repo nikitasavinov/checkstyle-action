9cbe909d106c0da795073956b06eec03aaa0c6e2
Nikita <savinovnickita@gmail.com>
2020-05-30 16:27:41 +0200

    Update Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -24,7 +24,7 @@ public class Application {
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
+			for (String beanName : beanNames) { 
 				System.out.println(beanName);
 			}
 		};
