e2364bd9e3a85242dd5c04726699cfecaf62314e
Nikita <savinovnickita@gmail.com>
2020-05-30 16:22:19 +0200

    Update Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -20,7 +20,7 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			System.out.println("Let's inspect the beans provided by Spring Boot:"); 
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
