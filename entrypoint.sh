8c6cea582f1712477017fb8d2a772271386976b2
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 16:22:04 +0800

    test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Running check(andy2)"
+echo "Running check(andy3)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 exec git config --global --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
-exec env
+env
 exec git diff-tree --no-commit-id --name-only -r ${GITHUB_REF} ${GITHUB_SHA} >changes.txt
 exec cat changes.txt
 exec cat /sample.xml \
