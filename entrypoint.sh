7dfc245f253132763871a654814dacebf3962404
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 21:30:46 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,14 @@ echo "Running check(andy7)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-exec git fetch origin ${GITHUB_BASE_REF}
-exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} > changes.txt
-echo "changes.txt generated"
+# exec git fetch origin ${GITHUB_BASE_REF}
+# exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} > changes.txt
+# echo "changes.txt generated"
 
-exec git clone https://github.com/andxu/java-checkstyle ../java-checkstyle
-exec cd ../java-checkstyle && git checkout develop && npm i && node src/checkstyle /github/workspace
-exec node /java-checkstyle/src/checkstyle /github/workspace
-exec cat /github/workspace/checkstyle-result.xml \
-exec cat /sample.xml \
- | reviewdog -f=checkstyle \
-      -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE:-added}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
-      -level="${INPUT_LEVEL}"
-
-
-
+# exec git clone https://github.com/andxu/java-checkstyle ../java-checkstyle
+# exec cd ../java-checkstyle && git checkout develop && npm i && node src/checkstyle /github/workspace
+# exec node /java-checkstyle/src/checkstyle /github/workspace
+# exec cat /github/workspace/checkstyle-result.xml \
 # exec cat /sample.xml \
 #  | reviewdog -f=checkstyle \
 #       -name="${INPUT_TOOL_NAME}" \
@@ -29,3 +19,13 @@ exec cat /sample.xml \
 #       -filter-mode="${INPUT_FILTER_MODE:-added}" \
 #       -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
 #       -level="${INPUT_LEVEL}"
+
+
+
+exec cat /sample.xml \
+ | reviewdog -f=checkstyle \
+      -name="${INPUT_TOOL_NAME}" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+      -filter-mode="${INPUT_FILTER_MODE:-added}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
+      -level="${INPUT_LEVEL}"
