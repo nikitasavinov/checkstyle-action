efd033241200ce3efd69cbc83a8f62e90ef1a55b
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 19:54:40 +0800

    test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "Running check(andy6)"
+echo "Running check(andy5)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+echo "git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} ${GITHUB_BASE_REF}"
+exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} ${GITHUB_BASE_REF} > changes.txt
+echo "changes.txt generated"
