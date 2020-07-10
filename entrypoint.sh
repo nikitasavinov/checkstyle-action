150b13e0a9e358c690c8b66c898d07bb9fb0d646
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 15:11:25 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@
 echo "Running check"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-exec java -jar /checkstyle.jar -c /"${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}" -f xml
-exec cat /sample.xml | reviewdog -f=checkstyle \
+
+exec java -jar /checkstyle.jar -c /"${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}" -f xml \
+ | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE:-added}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
       -level="${INPUT_LEVEL}"
-
-echo "Done"
