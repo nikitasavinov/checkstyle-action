7dc47f5f13e2ce408854b71b64c9487563425a13
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 21:10:39 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ echo "Running check(andy7)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-exec git fetch origin ${GITHUB_BASE_REF}
-exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} > changes.txt
-echo "changes.txt generated"
+# exec git fetch origin ${GITHUB_BASE_REF}
+# exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} > changes.txt
+# echo "changes.txt generated"
 
 # exec git clone https://github.com/andxu/java-checkstyle ../java-checkstyle
 # exec cd ../java-checkstyle && git checkout develop && npm i && node src/checkstyle /github/workspace
-exec node /java-checkstyle/src/checkstyle /github/workspace
+# exec node /java-checkstyle/src/checkstyle /github/workspace
 # exec cat /github/workspace/checkstyle-result.xml \
 exec cat /sample.xml \
  | reviewdog -f=checkstyle \
