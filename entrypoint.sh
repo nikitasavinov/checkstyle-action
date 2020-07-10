cfb646a2d880ce2523a917b171ea96a70c867afb
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 15:29:17 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Running check(andy)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-exec cat sample.xml \
+exec cat /sample.xml \
  | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
