afeb7d4fd289197d44e7856ece3a12048440ce54
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 19:53:22 +0800

    test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo "Running check(andy5)"
+echo "Running check(andy6)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-exec cat /sample.xml \
- | reviewdog -f=checkstyle \
-      -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE:-added}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
-      -level="${INPUT_LEVEL}"
