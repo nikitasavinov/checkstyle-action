58126f00ce4bbd4ee85ff777307529ba35d88221
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 16:12:42 +0800

    test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo "Running check(andy)"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+exec git diff-tree --no-commit-id --name-only -r ${GITHUB_REF} ${GITHUB_SHA} >changes.txt
+exec cat changes.txt
 exec cat /sample.xml \
  | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
