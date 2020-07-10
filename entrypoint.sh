95912cf069f663b8c120eafef1db61b03d333ed4
Andy Xu(devdiv) <andxu@microsoft>
2020-07-10 19:39:52 +0800

    test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 env
 echo "git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF}"
 git config --global --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
-exec git diff-tree --no-commit-id --name-only -r ${GITHUB_REF} ${GITHUB_SHA} >changes.txt
-echo $(cat changes.txt)
+exec git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} >changes.txt
+cat changes.txt
 exec cat /sample.xml \
  | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
