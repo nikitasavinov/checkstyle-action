d236d4d9a3563684368581f1127f7e4773013e9d
Nikita <savinovnickita@gmail.com>
2020-07-26 20:48:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Running check"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-exec java -jar /checkstyle.jar -c /"${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}" -f xml \
+exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}"  -f xml \
  | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
